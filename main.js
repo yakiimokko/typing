9d60693db900d823171a4a4f56c2ae54f15388fc
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-18 00:01:12 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+//ランダムなテキストを表示
+const createText = () => {};
+
+//キー入力の判定
+const keyPress = e => {};
+
+//タイピングスキルのランクを判定
+const rankCheck = score => {};
+
+//ゲーム終了
+const gameOver = id => {};
+
+//カウントダウンタイマー
+const timer = () => {};
